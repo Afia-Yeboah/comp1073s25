13a8c06c6679401f5b5f9aae08db1064d512517e
Afia Yeboah <aforiwaa2015@gmail.com>
2025-05-30 10:12:10 -0400

    Wk4/Half of conditionals statements for weather type and temperature

--- lesson-4/js/conditionals.js
+++ lesson-4/js/conditionals.js
@@ -1,8 +1,14 @@
 const output = document.querySelector('body p:nth-of-type(2)');
 		
 /* STEP 1a: A simple if/else statement */
-let homeWorkDone = null;
+let homeWorkDone = 1; // Can be bool values or bool num as stated in step 1b
+if (homeWorkDone === true) {
+	breakDuration = 60;
+} else {
+	breakDuration = 10;
+};
 
+output.textContent = `I get ${breakDuration} minutes for a break.`;
 // STEP 1b: Any value that is not false, undefined, null, 0, NaN, or an empty string will evaluate to TRUE when tested using a conditional statement - try a few other values for var homeWorkDone and test
 
 /* STEP 2: The more complex if|elseif|else */
@@ -14,12 +20,28 @@ const weatherButton = document.querySelector('#weatherButton');
 const comments = document.querySelector('#commentary');
 
 /* STEP 2a: Add an event listener for the 'click' event on the 'Set Weather' button that invokes the setWeather() function below */
-
+weatherButton.addEventListener("click", setWeather);
 
 function setWeather() {
 	let choice = select.value;
 	let temperature = temp.value;
+	console.log(`The weather is ${choice} and the temperature is ${temperature}.`);
 	/* STEP 2b: Craft an IF/ELSEIF/ELSE that changes the src attribute of the icon element to the appropriate .svg file in the images folder */
+	if (choice === "Sunny") {
+		icon.src = "images/sunny.svg";
+		// Color screen based on temperature
+		if (temperature >= 15) {
+			page.style.backgroundColor = "orange";
+		} else {
+			page.style.backgroundColor = "light blue";
+		};
+	} else if (choice === "rainy") {
+		icon.src = "images/rainy.svg";
+	} else if (choice === "windy") {
+		icon.src = "images/windy.svg";
+	} else {
+		icon.src = "images/cloud-off.svg";
+	}; // end of statement delimeter it's optional but best practice
 
 	/* STEP 3: Add a nested IF/ELSE statement inside the 'sunny' condition above that tests for temperature, and if it is equal to or greater than 15, turn the page background orange, otherwise turn it lightblue */
 
