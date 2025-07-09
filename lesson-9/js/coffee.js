c5db906e67efb17c18ecce6ec9cb5350c358ab99
Afia Yeboah <aforiwaa2015@gmail.com>
2025-07-09 18:56:07 -0400

    Wk9- Using Class instead of Constructors in creating Obj

--- lesson-9/js/coffee.js
+++ lesson-9/js/coffee.js
@@ -1,9 +1,31 @@
 const output = document.querySelector('#output');
 
 /* STEP 1: Instead of a constructor function, let's try a JavaScript class called 'Coffee' */
-
+class Coffee{
+    // first I need when creating a class is 
+    // variable/attributes
+    size;
+    isDecaf;
+    //Next is Constructor and pass the values we need
+    constructor(size, isDecaf){
+        this.size = size;
+        this.isDeacf = isDecaf;
+    };
+    //Next methods
+    // add a serveIt method
+    serveIt(){
+        // Generate an IMG of the coffee ordered
+        let cup = document.createElement("img"); // <img>
+        console.log(cup);
+        //Set the src path for the IMG element
+        // Determine caffeine status of the coffe
+        //Set the size of the cup SVG image based on this.size
+        //Generate a description of the coffe and put it into the IMG title attribute
+        // Insert the new IMG element into the paragraph
+    }
+};
 /* STEP 2: Instatiate a coffee based on the above constructor function */
-
+let afiaCoffee = new Coffee("medium", false);
 /* STEP 3: Add a method to the Coffee class called serveIt() */
 
 /* STEP 4: Call up the serveIt() method */
