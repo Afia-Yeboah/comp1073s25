9350a4b6a4f33c45a14099261ceb3e90d68035e6
Afia Yeboah <aforiwaa2015@gmail.com>
2025-07-09 19:36:57 -0400

    Wk9- Using the Class and extends

--- lesson-9/js/coffee.js
+++ lesson-9/js/coffee.js
@@ -16,24 +16,70 @@ class Coffee{
     serveIt(){
         // Generate an IMG of the coffee ordered
         let cup = document.createElement("img"); // <img>
-        console.log(cup);
+        //console.log(cup);
         //Set the src path for the IMG element
-        // Determine caffeine status of the coffe
+        // <img src="images/coffee.cup.svg"> setAttribute takes two attributes, Value and the info
+        cup.setAttribute("src", "images/coffee.cup.svg");
+        console.log(cup);
+        // Determine caffeine status of the coffee
+        // use if statements to check the different states of the item
+        if (this.isDecaf) {
+            // <img src="images/coffee-cup-green.svg">
+            cup.setAttribute("src", "images/coffee-cup-green.svg")
+        } else {
+            // <img src="images/coffee-cup-purple.svg">
+            cup.setAttribute ("src", "images/coffee-cup-purple.svg")
+        }
         //Set the size of the cup SVG image based on this.size
+        switch (this.size) {
+            case "small":
+                // <img src="images/coffee-cup-green.svg height = 100>
+                cup.setAttribute("height", "100");
+                break;
+            case "medium":
+                // <img src="images/coffee-cup-green.svg
+                cup.setAttribute("height", "150");
+                break;
+            case "large":
+                //<img src="images/coffee-cup-green.svg
+                cup.setAttribute("height", "200");
+                break;
+            default:
+                cup.setAttribute("height", "150");
+
+        }
         //Generate a description of the coffe and put it into the IMG title attribute
+        cup.setAttribute("title", `A ${this.size} sized coffe.`);
         // Insert the new IMG element into the paragraph
+        // Using Append to adds the Value to my Output in the Paragraph Tag
+        output.appendChild(cup);
     }
 };
 /* STEP 2: Instatiate a coffee based on the above constructor function */
 let afiaCoffee = new Coffee("medium", false);
+let batiyahCoffee = new Coffee("large", true);
+let zaneCoffee = new Coffee("small", false);
 /* STEP 3: Add a method to the Coffee class called serveIt() */
 
 /* STEP 4: Call up the serveIt() method */
+afiaCoffee.serveIt();
+batiyahCoffee.serveIt();
+zaneCoffee.serveIt();
 
-/* STEP 5: Define a subclass of the Coffee class */
-
+/* STEP 5: Define a subclass of the Coffee class NB: L in latte should always be Caps*/
+class Latte extends Coffee{
+    // Create Variable because it doesn't have in the Parent Class aside that, I don't need to recreate what I had done above
+    milkType;
+    constructor(size, isDecaf, milkType){
+        super(size, isDecaf);
+        this.mlikType = milkType;
+    }
+    latteDesc(){
+        alert (`A ${this.size} sized Latte with ${this.milkType} milk.`);
+    }
+}
 /* STEP 6: Create a new instance of the Latte object */
-
+let afiaLatte = new Latte("medium", false, "2%");
 /* STEP 7: Call up the latteDesc() method for the above created Latte instance */
 
 /* STEP 8: Create yet another instance of Latte using the console, and try the latteDesc() method from the subclass, as well as the serveIt() method from the parent class */
